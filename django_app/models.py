0c4b998c5cec02d6219b8dd57123a2b4e4417cdb
mcjug2015 <victor.semenov@gmail.com>
2016-09-20 20:44:51 -0400

    adding meeting, types, and admin user to db

--- django_app/models.py
+++ django_app/models.py
@@ -12,3 +12,45 @@ class CigarShop(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class MeetingType(models.Model):
+    ''' the type of meeting '''
+    short_name = models.CharField(null=False, blank=False, max_length=250)
+    name = models.CharField(null=False, blank=False, max_length=250)
+    description = models.CharField(null=False, blank=False, max_length=250)
+    def __str__(self):
+        return ("%s(%s)" % (self.name, self.short_name))
+
+
+class Meeting(models.Model):
+    ''' meeting models class '''
+
+    DAY_OF_WEEK_CHOICES = [(1, "Sunday"),
+                           (2, "Monday"),
+                           (3, "Tuesday"),
+                           (4, "Wednesday"),
+                           (5, "Thursday"),
+                           (6, "Friday"),
+                           (7, "Saturday")]
+
+    objects = models.GeoManager()
+    day_of_week = models.IntegerField(null=False, blank=False, choices=DAY_OF_WEEK_CHOICES)
+    start_time = models.TimeField(null=False, blank=False)
+    end_time = models.TimeField(null=False, blank=False)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    address = models.CharField(max_length=300)
+    creator = models.ForeignKey(User, related_name='meetings', null=True, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    geo_location = models.PointField()
+    types = models.ManyToManyField(MeetingType, related_name='meetings')
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        meeting_str = 'Meeting id: %s, ' % str(self.pk)
+        if self.creator:
+            meeting_str += "Creator: %s" % str(self.creator.username)
+        return meeting_str
