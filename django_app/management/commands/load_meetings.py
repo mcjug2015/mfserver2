1fc3b2629bf975fce39773a072d0c189f06d3e18
mcjug2015 <victor.semenov@gmail.com>
2017-06-15 22:04:54 -0400

    load_meetings command works

---
+++ django_app/management/commands/load_meetings.py
@@ -0,0 +1,75 @@
+''' module for the load meetings command '''
+# pylint: disable=too-many-locals
+import os
+import logging
+import json
+import requests
+from django.core.management.base import BaseCommand
+from django_app.services.auth_service import RefererTokenSessionHelper
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    ''' class for the command '''
+
+    def add_arguments(self, parser):
+        parser.add_argument('--meeting_file', default="/home/dtuser/Desktop/latest_meetings.txt")
+        parser.add_argument('--username', default='mf_admin')
+        parser.add_argument('--password', default='mf_admin')
+        parser.add_argument('--base_url', default='http://127.0.0.1:8000/')
+        parser.add_argument('--error_file', default=os.path.join(os.path.dirname(__file__),
+                                                                 '..', '..', '..', 'loader_error.txt'))
+        parser.add_argument('--error_detail_file', default=os.path.join(os.path.dirname(__file__),
+                                                                        '..', '..', '..', 'loader_error_detail.txt'))
+
+    def handle(self, *args, **options):
+        username = options['username']
+        password = options['password']
+        base_url = options['base_url']
+        meeting_file_path = options['meeting_file']
+        error_file_path = options['error_file']
+        error_detail_file_path = options['error_detail_file']
+        initial_url = "%s%s" % (base_url, "mfserver2/welcome/")
+        login_url = "%s%s" % (base_url, "mfserver2/login_async/")
+        post_meeting_url = "%s%s" % (base_url, "mfserver2/api/v1/savemeeting/")
+        helper = RefererTokenSessionHelper(username, password,
+                                           initial_url, login_url)
+        the_referer = helper.get_referer()
+        the_token, session_id = helper.get_token_session()
+        meeting_file_handle = open(meeting_file_path)
+        cookies = dict(csrftoken=the_token, sessionid=session_id)
+        headers = {"X-CSRFToken": the_token, "referer": the_referer,
+                   "Content-Type": "application/json"}
+        error_handle = open(error_file_path, 'w')
+        error_detail_handle = open(error_detail_file_path, 'w')
+        meetings_found = 0
+        meetings_succeeded = 0
+        for line in meeting_file_handle:
+            stripped_line = line.strip()
+            if stripped_line:
+                meetings_found += 1
+                response = None
+                try:
+                    meeting_obj = json.loads(line)
+                    response = requests.post(post_meeting_url,
+                                             json.dumps(meeting_obj),
+                                             cookies=cookies,
+                                             headers=headers,
+                                             verify=False)
+                    if response.status_code == 201:
+                        meetings_succeeded += 1
+                    else:
+                        raise  # pylint: disable=misplaced-bare-raise
+                except:  # pylint: disable=bare-except
+                    error_handle.write(stripped_line + '\n')
+                    error_detail_handle.write('%s\n%s\n%s\n\n' % (stripped_line,
+                                                                  response.status_code if response is not None else "",
+                                                                  response.text if response is not None else ""))
+        meeting_file_handle.close()
+        error_handle.close()
+        error_detail_handle.close()
+        meetings_failed = meetings_found - meetings_succeeded
+        LOGGER.info("Ran %s commands, of them %s succeeded and %s failed",
+                    meetings_found, meetings_succeeded, meetings_failed)
---
+++ django_app/test/commands/load_meetings_tests.py
@@ -0,0 +1,62 @@
+''' tests module for the load meetings command '''
+from tempfile import mkdtemp
+import shutil
+import os
+from django.test.testcases import TestCase
+from django_app.management.commands import load_meetings
+from mockito.mockito import unstub, verify, when
+from mockito.matchers import any  # pylint: disable=redefined-builtin
+from mockito.mocking import mock
+
+
+class LoadMeetingsTests(TestCase):
+    ''' test class for the load meetings command '''
+
+    def setUp(self):
+        ''' set up the test '''
+        self.work_dir = mkdtemp()
+        self.command = load_meetings.Command()
+
+    def tearDown(self):
+        ''' tear down test '''
+        unstub()
+        shutil.rmtree(self.work_dir)
+
+    def test_add_arguments(self):
+        ''' test adding arguements '''
+        the_parser = mock()
+        when(the_parser).add_argument(any(), default=any()).thenReturn(None)
+        self.command.add_arguments(the_parser)
+        verify(the_parser, times=6).add_argument(any(), default=any())
+
+    def test_handle(self):
+        ''' verify that good and failing meetings act appropriately '''
+        when(load_meetings.RefererTokenSessionHelper).get_referer().thenReturn("test_referer")
+        when(load_meetings.RefererTokenSessionHelper).get_token_session().thenReturn(("test_token",
+                                                                                      "test_session"))
+        meeting_file_path = os.path.join(os.path.dirname(__file__), '..', 'res', 'test_meetings_to_load.txt')
+        error_file_path = os.path.join(self.work_dir, "test_error.txt")
+        error_detail_file_path = os.path.join(self.work_dir, "test_error_detail.txt")
+        response1 = mock()
+        response1.status_code = 201
+        response2 = mock()
+        response2.status_code = 500
+        response2.text = "test fail response text"
+        when(load_meetings.requests).post(any(), any(), cookies=any(),
+                                          headers=any(), verify=False).thenReturn(response1, response2)
+        self.command.handle(username="a", password="b", base_url="c",
+                            meeting_file=meeting_file_path,
+                            error_file=error_file_path,
+                            error_detail_file=error_detail_file_path)
+        error_output = open(error_file_path).read()
+        error_detail_output = open(error_detail_file_path).read()
+        verify(load_meetings.RefererTokenSessionHelper).get_referer()
+        verify(load_meetings.RefererTokenSessionHelper).get_token_session()
+        verify(load_meetings.requests, times=2).post(any(), any(), cookies=any(), headers=any(),
+                                                     verify=False)
+        self.assertIn("-777777777.0", error_output)
+        self.assertIn("zzzzz", error_output)
+        self.assertIn("-777777777.0", error_detail_output)
+        self.assertIn("500", error_detail_output)
+        self.assertIn("test fail response text", error_detail_output)
+        self.assertIn("zzzzz", error_detail_output)
